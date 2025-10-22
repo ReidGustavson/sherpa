be7b27dbd821b8cc745b4087ba695e1221bc8bd9
Reid <reidgust@gmail.com>
2025-10-22 11:51:11 -0700

    fix math

--- src/pages/Sudoku/SudokuGame/CubeMath.tsx
+++ src/pages/Sudoku/SudokuGame/CubeMath.tsx
@@ -31,7 +31,7 @@ export function checkForSolve(state: number[]): boolean {
         depth.add(state[k * gameSize * gameSize + j * gameSize + i]);
       }
       for (const dimension of [row, column, depth]) {
-        if (dimension.has(gameSize) || dimension.size < gameSize) {
+        if (dimension.size < gameSize) {
           return false
         }
       }
